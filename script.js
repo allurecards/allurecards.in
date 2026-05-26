4fdb33642275da69851373b16db35326616b0087
allurecards <allurecards.in@gmail.com>
2026-05-26 18:50:47 +0530

    Add styling to product ID display

--- script.js
+++ script.js
@@ -267,7 +267,7 @@ if (header) {
                         </button>
                     </div>
                 </div>
-                <h4 class="product-id">${escapeHtml(product.id)}</h4>
+                <h4 class="product-id" style="font-family: 'Courier New', monospace; letter-spacing: 0.02em;">${escapeHtml(product.id)}</h4>
                 <p class="product-price">Rs. ${product.price} / card</p>
             </div>
         `;
